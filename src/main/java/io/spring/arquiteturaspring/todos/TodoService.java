c2d4197970c9a4d3fbfb1daafa141b1b2bb24f55
GustavoPBrum <brumtavo@gmail.com>
2025-06-10 12:24:41 +0100

    Codificando as camadas de Arquitetura

---
+++ src/main/java/io/spring/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,19 @@
+package io.spring.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    // Como eh uma classe gerenciada pelo Spring (por ser um service) nao precisamos do Autowired, o proprio Spring vai procurar dentro do container
+    // Uma instancia de TodoRepository para injetar neste construtor automaticamente
+    public TodoService(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoEntity) {
+        return repository.save(novoEntity);
+    }
+}
